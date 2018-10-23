c707bb22f8f5333294d725b27a78f197e4c7258e
BrentBauman86 <brentbauman86@gmail.com>
2018-10-22 19:55:43 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments, reject_if: proc {|attributes| attributes.all? { |key, value| key == "_destroy" || value.blank?}}
 
+  def comment_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment 
+    end
+  end
 
 end
